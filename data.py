696d1c313e6e0551ffc47d8c3edf66adba027af2
ahoarfrost <adrienne.l.hoarfrost@gmail.com>
2019-11-26 14:04:03 -0500

    add max_seqs_per_file to read first N seqs from a file into itemlist

--- data.py
+++ data.py
@@ -20,14 +20,18 @@ def check_seqfiletype(filename:PathOrStr, extensions:Collection[str]=supported_s
     assert seqfiletype in extensions, "Input sequence file type %r is not supported." % seqfiletype
     return seqfiletype
 
-def get_items_from_seqfile(filename:PathOrStr, extensions:Collection[str]=supported_seqfiletypes):
+def get_items_from_seqfile(filename:PathOrStr, extensions:Collection[str]=supported_seqfiletypes, max_seqs:int=None):
     seqfiletype = check_seqfiletype(filename, extensions)
     iterator = seqfiletype_to_iterator[seqfiletype]
     with open(filename, "r") as handle:
         items = []
+        row = 0
         for title, seq, qual, offset in iterator(handle):
             #get (filename, offset, length of sequence) for each read and add to items
             items.append(seq)
+            row += 1
+            if max_seqs and row >= max_seqs: 
+                break
     handle.close()
     return items
 
@@ -36,29 +40,29 @@ class BioTextList(TextList):
         super().__init__(items, **kwargs)
        
     @classmethod
-    def from_seqfile(cls, filename:PathOrStr, path:PathOrStr='.', extensions:Collection[str]=supported_seqfiletypes, **kwargs)->'TextList':
+    def from_seqfile(cls, filename:PathOrStr, path:PathOrStr='.', extensions:Collection[str]=supported_seqfiletypes, max_seqs_per_file:int=None, **kwargs)->'TextList':
         "Creates a SeqList from a single sequence file (e.g. .fastq, .fasta, etc.)"
         #get (filename, offset) tuple for each read and add to items
-        items = get_items_from_seqfile(filename=filename, extensions=extensions)
+        items = get_items_from_seqfile(filename=filename, extensions=extensions, max_seqs=max_seqs_per_file)
         
         return cls(items=items, path=path, **kwargs)
 
     @classmethod
-    def from_folder(cls, path:PathOrStr='.', extensions:Collection[str]=supported_seqfiletypes, recurse:bool=True, **kwargs) -> 'TextList':
+    def from_folder(cls, path:PathOrStr='.', extensions:Collection[str]=supported_seqfiletypes, max_seqs_per_file:int=None, recurse:bool=True, **kwargs) -> 'TextList':
         "Creates a SeqList from all sequence files in a folder"
         #get list of files in `path` with seqfile suffixes. `recurse` determines if we search subfolders.
         files = get_files(path=path, extensions=extensions, recurse=recurse)
         #within each file, get (filename, offset, length of sequence) for each read and add to items
         items = []
         for filename in files:
-            items.extend(get_items_from_seqfile(filename=filename, extensions=extensions))
+            items.extend(get_items_from_seqfile(filename=filename, extensions=extensions, max_seqs=max_seqs_per_file))
 
         print(items)
         return cls(items=items, path=path, **kwargs)
 
 class BioLMDataBunch(TextLMDataBunch):
     @classmethod
-    def from_seqfile(cls, path:PathOrStr, filename:PathOrStr, extensions:Collection[str]=supported_seqfiletypes, 
+    def from_seqfile(cls, path:PathOrStr, filename:PathOrStr, extensions:Collection[str]=supported_seqfiletypes, max_seqs_per_file:int=None,
                         valid_pct:float=0.2, 
                         bptt=70, bs=64, seed:int=None, collate_fn:Callable=data_collate, device:torch.device=None, no_check:bool=False, backwards:bool=False,
                         vocab:BioVocab=None, tokenizer:BioTokenizer=None, 
@@ -68,7 +72,7 @@ class BioLMDataBunch(TextLMDataBunch):
         processor = get_lol_processor(tokenizer=tokenizer, vocab=vocab, chunksize=chunksize, max_vocab=max_vocab,
                                    min_freq=min_freq, include_bos=include_bos, include_eos=include_eos)
 
-        src = BioTextList.from_seqfile(filename=filename, path=path, extensions=extensions, processor=processor)
+        src = BioTextList.from_seqfile(filename=filename, path=path, extensions=extensions, max_seqs_per_file=max_seqs_per_file, processor=processor)
         src = src.split_by_rand_pct(valid_pct=valid_pct, seed=seed)
         src = src.label_for_lm()
 
@@ -85,7 +89,7 @@ class BioLMDataBunch(TextLMDataBunch):
         return cls(*dls, path=path, device=device, collate_fn=collate_fn, no_check=no_check)
 
     @classmethod
-    def from_folder(cls, path:PathOrStr, extensions:Collection[str]=supported_seqfiletypes, recurse:bool=True, 
+    def from_folder(cls, path:PathOrStr, extensions:Collection[str]=supported_seqfiletypes, recurse:bool=True, max_seqs_per_file:int=None,
                     valid_pct:float=0.2, train:str=None, valid:str=None, test:Optional[str]=None,
                     tokenizer:BioTokenizer=None, vocab:BioVocab=None, collate_fn:Callable=data_collate, device:torch.device=None, no_check:bool=False, backwards:bool=False,
                     chunksize:int=10000, max_vocab:int=60000, min_freq:int=2, include_bos:bool=None, include_eos:bool=None, 
@@ -96,7 +100,7 @@ class BioLMDataBunch(TextLMDataBunch):
                                    min_freq=min_freq, include_bos=include_bos, include_eos=include_eos)
                                    
         
-        src = BioTextList.from_folder(path=path, extensions=extensions, recurse=recurse, processor=processor)
+        src = BioTextList.from_folder(path=path, extensions=extensions, recurse=recurse, max_seqs_per_file=max_seqs_per_file, processor=processor)
         if valid_pct:
             src = src.split_by_rand_pct(valid_pct=valid_pct, seed=seed)
         else:
@@ -118,7 +122,7 @@ class BioLMDataBunch(TextLMDataBunch):
 
 class BioClasDataBunch(TextClasDataBunch):
     @classmethod
-    def from_folder(cls, path:PathOrStr, extensions:Collection[str]=supported_seqfiletypes, recurse:bool=True, 
+    def from_folder(cls, path:PathOrStr, extensions:Collection[str]=supported_seqfiletypes, recurse:bool=True, max_seqs_per_file:int=None,
                     valid_pct:float=0.2, train:str=None, valid:str=None, test:Optional[str]=None,
                     classes:Collection[str]=None, pad_idx:int=1, pad_first:bool=True, 
                     device:torch.device=None, backwards:bool=False, no_check:bool=False,
@@ -130,7 +134,7 @@ class BioClasDataBunch(TextClasDataBunch):
         processor = get_lol_processor(tokenizer=tokenizer, vocab=vocab, chunksize=chunksize, max_vocab=max_vocab,
                                    min_freq=min_freq, include_bos=include_bos, include_eos=include_eos)
         
-        src = BioTextList.from_folder(path=path, extensions=extensions, recurse=recurse, processor=processor)
+        src = BioTextList.from_folder(path=path, extensions=extensions, recurse=recurse, max_seqs_per_file=max_seqs_per_file, processor=processor)
         if valid_pct:
             src = src.split_by_rand_pct(valid_pct=valid_pct, seed=seed)
         else:
@@ -153,7 +157,7 @@ class BioClasDataBunch(TextClasDataBunch):
         return cls(*dataloaders, path=path, device=device, collate_fn=collate_fn, no_check=no_check)
 
     @classmethod
-    def from_seqfile(cls, path:PathOrStr, extensions:Collection[str]=supported_seqfiletypes, recurse:bool=True, 
+    def from_seqfile(cls, path:PathOrStr, extensions:Collection[str]=supported_seqfiletypes, recurse:bool=True, max_seqs_per_file:int=None,
                     valid_pct:float=0.2, label_func:Callable=None,
                     classes:Collection[str]=None, pad_idx:int=1, pad_first:bool=True, backwards:bool=False,
                     device:torch.device=None, no_check:bool=False,
@@ -166,7 +170,7 @@ class BioClasDataBunch(TextClasDataBunch):
         processor = get_lol_processor(tokenizer=tokenizer, vocab=vocab, chunksize=chunksize, max_vocab=max_vocab,
                                    min_freq=min_freq, include_bos=include_bos, include_eos=include_eos)
 
-        src = BioTextList.from_seqfile(filename=filename, path=path, extensions=extensions, processor=processor)
+        src = BioTextList.from_seqfile(filename=filename, path=path, extensions=extensions, max_seqs_per_file=max_seqs_per_file, processor=processor)
         src = src.split_by_rand_pct(valid_pct=valid_pct, seed=seed)
         if label_func:
             src = src.label_from_func(label_func)
