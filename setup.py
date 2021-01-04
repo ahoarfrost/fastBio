7de5f04f96fff6d531bf3bb0646932f8c9f169de
Adrienne Hoarfrost <adrienne.l.hoarfrost@gmail.com>
2021-01-04 14:40:04 -0500

    add dependencies

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="fastBio", 
-    version="0.1",
+    name="fastBio",
+    version="0.1.1",
     author="Adrienne Hoarfrost",
     author_email="adrienne.l.hoarfrost@gmail.com",
     description="Deep learning for biological sequences with fastai",
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/ahoarfrost/fastBio",
     packages=setuptools.find_packages(),
+    install_requires = ['fastai==1.0.52', 'biopython==1.73'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
